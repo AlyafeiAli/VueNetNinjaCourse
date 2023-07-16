07479f4218dd111776c2e41dc8f8dfecaedd5e86
AlyafeiAli <theghost970@gmail.com>
2023-07-16 17:12:46 +0400

    onClick events

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const app = Vue.createApp({
       title: "Harry Potter",
       author: "J.K. Rowling",
       date: 2001,
+      age: 0,
     };
+  },
+  methods: {
+    changeTitle() {
+      this.title =
+        this.title == "Harry Potter" ? "Cursed Child" : "Harry Potter";
+    },
   },
 });
 app.mount("#app"); //control everthing inside the div with this ID
