8447cca6745c39e07949a62d0467ed2a8bbd479c
AlyafeiAli <theghost970@gmail.com>
2023-07-16 17:34:08 +0400

    Conditional Rendering

--- app.js
+++ app.js
@@ -6,12 +6,16 @@ const app = Vue.createApp({
       author: "J.K. Rowling",
       date: 2001,
       age: 0,
+      showBooks: true,
     };
   },
   methods: {
     changeTitle() {
       this.title =
         this.title == "Harry Potter" ? "Cursed Child" : "Harry Potter";
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
